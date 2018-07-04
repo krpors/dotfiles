c81b559519af8e11d259093a9016481f342223b3
Kevin Pors <kevin.pors@ing.nl>
2018-07-04 13:00:51 +0200

    added install script
    got tired of manual copying and all that jazz

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+
+printf "This script installs the files in this repository into the current\n"
+printf "user's home directory.\n"
+printf "\n"
+printf "The installation directory will be:\n"
+printf "    %s\n" ${HOME}
+printf "\n"
+
+function install {
+	rsync -a --exclude="install.sh" --exclude=".git" . ~/
+}
+
+read -p "Continue? (y/n): " cont
+if [ "${cont}" == "y" ]; then
+	install
+	printf "Done!\n"
+else
+	printf "Exiting without any action.\n"
+	exit 0
+fi
+
