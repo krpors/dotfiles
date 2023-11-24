52d116158674091b32605f319f3a284cd356d720
Kevin Pors <krpors@gmail.com>
2023-11-24 12:40:18 +0100

    fish_add_path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,24 +1,17 @@
 set JAVA_HOME ~/Apps/jdk-11.0.4+11
 set GOPATH ~/Development/go
 set GOROOT ~/Apps/go
-set NODE_HOME ~/Apps/node-v10.16.3-linux-x64
-set HAXE_HOME ~/Apps/haxe
 set LOVE_HOME ~/Apps/love2d
 set LUAROCKS_HOME ~/.luarocks
-set SONAR_SCANNER_HOME ~/Apps/sonar-scanner-4.2.0.1873-linux
-set NIM_HOME ~/Apps/nim-1.0.6
-set M2_HOME ~/Apps/maven
+set M2_HOME ~/Apps/apache-maven-3.5.2
 
-set PATH $PATH $JAVA_HOME/bin
-set PATH $PATH $GOROOT/bin
-set PATH $PATH $GOPATH/bin
-set PATH $PATH $M2_HOME/bin
-set PATH $PATH $NODE_HOME/bin
-set PATH $PATH $LOVE_HOME
-set PATH $PATH $LUAROCKS_HOME/bin
-set PATH $PATH $SONAR_SCANNER_HOME/bin
-set PATH $PATH ~/.cargo/bin
-set PATH $PATH $NIM_HOME/bin
+fish_add_path $JAVA_HOME/bin
+fish_add_path $GOROOT/bin
+fish_add_path $GOPATH/bin
+fish_add_path $M2_HOME/bin
+fish_add_path $LOVE_HOME
+fish_add_path ~/.cargo/bin
+fish_add_path ~/.nimble/bin
 
 set TMOUT 0
 
